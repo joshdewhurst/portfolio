9b447a991a170a0811dad1efffd21d30c405edc6
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2024-07-16 19:28:01 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-// next.config.js
 module.exports = {
   images: {
-    domains: ['th.bing.com', 'clipground.com'],
+    domains: [
+      'www.sfaf.org',
+      'images.squarespace-cdn.com',
+      'www.tobenerlaw.com',
+      'www.ghitterman.com',
+      'secure.cada1.org',
+      'media.licdn.com',
+      'th.bing.com',
+      'clipground.com'
+    ],
   },
 }
